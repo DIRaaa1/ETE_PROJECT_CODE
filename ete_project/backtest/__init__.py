2477f65ef9019da2cbe5835bb22d0dd217a894ee
Xinkai04 <xinkail0403@gmail.com>
2026-08-26 19:00:10 +0800

    Delete ete_project directory

--- ete_project/backtest/__init__.py
+++
@@ -1,8 +0,0 @@
-from .backtest import load_config, main, run_backtest, run_backtest_from_config
-
-__all__ = [
-    "load_config",
-    "main",
-    "run_backtest",
-    "run_backtest_from_config",
-]
